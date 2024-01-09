1683735207eb78c89e730e26ca65325a925fca98
Alvaro Araoz <alvaroaraoz_92@hotmail.com>
2024-01-09 14:17:34 +0100

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,31 +11,20 @@ export default function Home() {
       >
         <PhotoIcon className="w-16 h-16 mx-auto" />
         <h1 className="text-xl md:text-2xl uppercase font-bold">
-          Bulk photos compressor
+          IMGfy
         </h1>
       </header>
       <HomeIsland />
       <footer className="absolute bottom-0 w-full p-4 dark:bg-slate-800 bg-slate-200">
         <nav className="flex flex-col sm:flex-row flex-wrap justify-end items-center gap-2 sm:gap-4 text-sm">
-          <BuyMeACoffee />
           <a
-            href="https://github.com/negrel/bulkcompress.photos"
-            className="underline"
-            target="_blank"
-          >
-            Source code
-          </a>
-          <a href="https://www.negrel.dev/#contact" className="underline">
-            Feedback
-          </a>
-          <a
-            href="https://www.negrel.dev"
+            href="https://imgfy.digital"
             className="underline"
             target="_blank"
             rel="author"
           >
-            Made with <span className="text-red-500">❤️</span>{" "}
-            by Alexandre Negrel
+            © 2024 IMGfy. Made with love <span className="text-red-500">❤️</span>{" "}
+            in Spain.
           </a>
         </nav>
       </footer>
